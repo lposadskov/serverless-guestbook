a599f5e68a2f6d39b3464caafc51f98573c10d13
lposadskov <lposadskov@gmail.com>
2021-05-30 01:50:40 +0900

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://f6a6644c.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
